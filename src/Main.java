df0142083cc54f3b8ba552123355888b5f515922
franciscossilva <francisco.dossantos@zup.com.br>
2023-06-03 00:21:35 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int soma = 0;
+        for (int i = 0;i < numeros.length;+i++){
+            soma += numeros[i];
+
+        }
+        System.out.println("A soma dos 20 numeros é "+ soma);
+
+
+
+
+
+
+    }
+}
